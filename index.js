393529b34a881ad4677907c292023bb27bc11ef9
Jesús Isidro Regules Santiago <isc20350312@gmail.com>
2023-09-14 13:54:54 -0600

    tercer commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) =>{
+    response.send('Hola GET')
+})
+app.post('/', (request, response) =>{
+    response.send('Hola POST')
+})
+
+app.put('/', (request, response) =>{
+    response.send('Hola PUT')
+})
+
+app.patch('/', (request, response) =>{
+    response.send('Hola PATCH')
+})
+
+app.delete('/', (request, response) =>{
+    response.send('Hola DELETE')
+})
+
+//http://localhost:3000/
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
